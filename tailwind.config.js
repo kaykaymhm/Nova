518bf867eb9fb0c25e0b77f694c891de1b035315
kaykaymhm <sulimantarawneh93@gmail.com>
2025-03-18 15:21:37 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import {heroui} from "@heroui/theme"
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+        righteous: ["var(--font-righteous)", "sans-serif"],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+}
+
+module.exports = config;
